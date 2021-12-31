366f2f6b4a33c58dfee99589f220d9d958061fe0
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2021-12-31 19:03:56 +0530

    Create Tower_of_Hanoi.java

---
+++ Recursion/Tower_of_Hanoi.java
@@ -0,0 +1,14 @@
+class Main
+{
+  public static void TOH(int n, char A, char B, char C)
+  {
+    if(n==1)
+    {
+      System.out.println("Move 1 from "+A+" to "+C);
+      return;
+    }
+    TOH(n-1,A,C,B);
+    System.out.println("Move "+n+" from "+A+" to "+C);
+    TOH(n-1,B,A,C);
+  }
+}
