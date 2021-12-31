ef7ad64c16f325cfff6318854eb7ac47d38ca46b
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2021-12-31 18:55:47 +0530

    Create LCM-of-2nos.java

---
+++ LCM-of-2nos.java
@@ -0,0 +1,14 @@
+class Main
+{
+  public static int gcd(int a, int b)
+  {
+    if(b==0)
+      return a;
+    return gcd(b,a%b);
+  }
+  public static int lcm(int a, int b)
+  {
+    return (a*b)/gcd(a,b);
+  }
+}
+//Time complexity= O(log(min(a,b)))
