9dfa620dcc2d38c37bc429d608d12dd247b260ca
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2021-12-31 22:35:38 +0530

    Create Lucky_numbers.java

---
+++ Recursion/Lucky_numbers.java
@@ -0,0 +1,33 @@
+/*
+Lucky numbers are subset of integers. Rather than going into much theory, let us see the process of arriving at lucky numbers,
+Take the set of integers
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,……
+First, delete every second number, we get following reduced set.
+1, 3, 5, 7, 9, 11, 13, 15, 17, 19,…………
+Now, delete every third number, we get
+1, 3, 7, 9, 13, 15, 19,….….
+Continue this process indefinitely……
+Any number that does NOT get deleted due to above process is called “lucky”.*/
+
+class Solution
+{
+    // Complete the function
+    // n: Input n
+    // Return True if the given number is a lucky number else return False
+    public static boolean isLucky(int n)
+    {
+        // Your code here
+        int ct = 2;
+        return fncall(n,ct);
+        
+    }
+    public static boolean fncall(int n, int ct) {
+        if (ct > n)
+        return true;
+        if (n % ct == 0)
+        return false;
+        n = n - (n/ct);
+        ct++;
+        return fncall(n, ct);
+    }
+}
