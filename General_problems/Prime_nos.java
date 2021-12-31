6ac7b54faf0d4b2dd4e20fbba6b785aab1d42206
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2021-12-31 18:59:01 +0530

    Create Prime_nos.java

---
+++ General_problems/Prime_nos.java
@@ -0,0 +1,13 @@
+//Time complexity = O(N^0.5)
+class Main
+{
+  public static boolean isPrime(int n)
+  {
+    if(n==1)
+      return false;
+    for(int i=2;i*i<=n;i++)
+      if(n%i==0)
+        return false;
+    return true;
+  }
+}
