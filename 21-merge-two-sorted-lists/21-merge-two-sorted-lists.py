b1b1c850571e550fc46bf04a54cd086f6927b769
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2022-09-08 18:16:38 +0530

    Time: 50 ms (15.32%), Space: 13.5 MB (31.62%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummyhead=ListNode(0)
+        curr=dummyhead
+        while list1!=None and list2!=None:
+            if list1.val<list2.val:
+                newNode=ListNode(list1.val)
+                curr.next=newNode
+                curr=newNode
+                list1=list1.next if list1 else None
+            elif list2.val<list1.val:
+                newNode=ListNode(list2.val)
+                curr.next=newNode
+                curr=newNode
+                list2=list2.next if list2 else None
+            else:
+                newNode=ListNode(list1.val)
+                curr.next=newNode
+                curr=newNode
+                list1=list1.next if list1 else None
+                newNode=ListNode(list2.val)
+                curr.next=newNode
+                curr=newNode
+                list2=list2.next if list2 else None
+        if list1!=None:
+            while list1:
+                newNode=ListNode(list1.val)
+                curr.next=newNode
+                curr=newNode
+                list1=list1.next if list1 else None
+        elif list2!=None:
+            while list2:
+                newNode=ListNode(list2.val)
+                curr.next=newNode
+                curr=newNode
+                list2=list2.next if list2 else None
+        return dummyhead.next
